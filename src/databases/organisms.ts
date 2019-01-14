913de42fea19766cb7a85b37b23eb2a7c1c767e1
Matt Dubin <medubin@gmail.com>
2019-01-13 23:25:24 -0800

    display number of alive and dead

--- src/databases/organisms.ts
+++ src/databases/organisms.ts
@@ -8,11 +8,15 @@ export default class Organisms {
   protected maxUuid : number
   protected _organisms : OrganismsInterface
   public length : number
+  public alive : number
+  public dead : number
 
   constructor() {
     this.maxUuid = 0
     this._organisms = {}
     this.length = 0
+    this.alive = 0
+    this.dead = 0
   }
 
   public getOrganism(uuid : string) : Organism {
@@ -22,9 +26,11 @@ export default class Organisms {
   public addOrganism(organism : Organism) {
     this._organisms[organism.uuid] = organism
     this.length += 1
+    this.alive += 1
   }
 
   public deleteOrganism(uuid : number) {
+    this._organisms[uuid].isAlive ? this.alive -= 1 : this.dead -= 1
     delete this._organisms[uuid]
     this.length -= 1
   }
--- src/services/world_display.ts
+++ src/services/world_display.ts
@@ -4,6 +4,8 @@ export default class WorldDisplay {
   protected database : Database
   protected stats : HTMLElement
   protected organismCount : HTMLElement
+  protected aliveCount : HTMLElement
+  protected deadCount : HTMLElement
   protected co2 : HTMLElement
   protected o2 : HTMLElement
   protected age : HTMLElement
@@ -16,6 +18,18 @@ export default class WorldDisplay {
     this.organismCount = document.createElement('span')
     organismText.appendChild(this.organismCount)
     this.stats.appendChild(organismText)
+
+    const aliveText = document.createElement('span')
+    aliveText.innerText = 'Alive: '
+    this.aliveCount = document.createElement('span')
+    aliveText.appendChild(this.aliveCount)
+    this.stats.appendChild(aliveText)
+
+    const deadText = document.createElement('span')
+    deadText.innerText = 'Dead: '
+    this.deadCount = document.createElement('span')
+    deadText.appendChild(this.deadCount)
+    this.stats.appendChild(deadText)
 
     const co2Text = document.createElement('span')
     co2Text.innerText = 'CO2: '
@@ -37,8 +51,10 @@ export default class WorldDisplay {
   }
 
   public tick() : void {
-    if (this.database.world.tickNumber % 100 === 0) {
+    if (this.database.world.tickNumber % 10 === 0) {
       this.organismCount.innerText = this.database.organisms.length.toString()
+      this.aliveCount.innerText = this.database.organisms.alive.toString()
+      this.deadCount.innerText = this.database.organisms.dead.toString()
       this.co2.innerText = Math.floor(this.database.world.co2).toString()
       this.o2.innerText = Math.floor(this.database.world.o2).toString()
       this.age.innerText = (this.database.world.tickNumber / 100).toString()
